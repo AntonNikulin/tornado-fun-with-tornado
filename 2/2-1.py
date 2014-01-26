d9e69f85949f4b10b921af3c569053cdfbc048d1
Anton Nikulin <dr.tallin@gmail.com>
2014-01-26 19:56:02 +0200

    Simple calc with form and template

--- 2/2-1.py
+++ 2/2-1.py
@@ -12,19 +12,18 @@ class IndexHandler(tornado.web.RequestHandler):
 	def get(self):
 		self.render("index.html")
 
-class PoemPageHandler(tornado.web.RequestHandler):
+class CalcPageHandler(tornado.web.RequestHandler):
 	def post(self):
-		noun1 = self.get_argument("noun1")
-		noun2 = self.get_argument("noun2")
-		verb = self.get_argument("verb")
-		noun3 = self.get_argument("noun3")
-		self.render("poem.html", roads=noun1, wood=noun2, made=verb, 
-			difference=noun3)
+		var1 = self.get_argument("var1")
+		var2 = self.get_argument("var2")
+		res = int(var1) + int(var2)
+		d ={"var1": var1, "var2": var2, "res": res}
+		self.render("result.html", var1 = var1, var2 = var2, res= res)
 
 if __name__ == "__main__":
 	tornado.options.parse_command_line()
 	app = tornado.web.Application(
-		handlers=[(r"/", IndexHandler),(r"/poem", PoemPageHandler)],
+		handlers=[(r"/", IndexHandler),(r"/calc", CalcPageHandler)],
 		template_path = os.path.join(os.path.dirname(__file__), "templates")
 	)
 	http_server = tornado.httpserver.HTTPServer(app)
